2d039343247c91111469954eee0e4561b991fc24
kretininavera6-ux <kretininavera6@gmail.com>
2025-11-04 22:00:20 +0300

    Add files via upload

---
+++ Source1.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+int main() {
+    setlocale(LC_ALL, "rus");
+    double x, numerator = 1.0, denominator = 1.0;
+    printf("Введите x: ");
+    scanf("%lf", &x);
+    for (int n = 1; n <= 6; n++) {
+        numerator *= (x - ((1 << n) - 1));
+        denominator *= (x - (1 << n));
+    }
+    printf("Результат: %lf\n", numerator / denominator);
+    return 0;
+}
